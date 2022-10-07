9da01a002058941728263715ec851eb467c50bdf
KuzmaEV <yaugen93@yandex.ru>
2022-10-07 18:14:48 +0300

    Made tests PizzaInfoService

---
+++ Pizzaria/src/test/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/services/PizzaInfoServiceTest.java
@@ -0,0 +1,28 @@
+package by.it_academy.jd2.Mk_JD2_92_22.pizza.services;
+
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.api.IPizzaInfo;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.menu.PizzaInfo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PizzaInfoServiceTest {
+
+    @Test
+    public void get(){
+        PizzaInfoService service = PizzaInfoService.getInstance();
+
+        IPizzaInfo pizzaInfo = service.get("Margarita");
+
+        Assertions.assertEquals("tomatoes", pizzaInfo.getDescription());
+    }
+
+    @Test
+    public void validate(){
+        PizzaInfoService service = PizzaInfoService.getInstance();
+        IPizzaInfo pizzaInfo = new PizzaInfo("Mexican", "pepper", 40);
+
+        service.validate(pizzaInfo);
+
+        Assertions.assertEquals(pizzaInfo.getDescription(), service.get("Mexican").getDescription());
+    }
+}
