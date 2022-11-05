4011ef25dd7397d8d57096e3dba8968780dc627c
KuzmaEV <yaugen93@yandex.ru>
2022-11-05 19:50:48 +0300

    Exception PizzaInfoDao

--- Pizzaria/src/main/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/dao/PizzaInfoDao.java
+++ Pizzaria/src/main/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/dao/PizzaInfoDao.java
@@ -109,8 +109,7 @@ public class PizzaInfoDao implements IPizzaInfoDao {
             int countUpdatedRows = stm.executeUpdate();
             if (countUpdatedRows != 1){
                 if (countUpdatedRows == 0){
-                    System.out.println("Несмогли обновить запись!");
-                    //throw new IllegalArgumentException( "Несмогли обновить запись!");
+                    throw new IllegalArgumentException( "Несмогли обновить запись!");
                 }else {
                     throw new IllegalArgumentException("Обнавили более одной записи!");
                 }
