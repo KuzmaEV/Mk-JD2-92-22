d230c9fd3aa21d514b60959646485e99f53894ec
KuzmaEV <yaugen93@yandex.ru>
2022-11-06 08:45:13 +0300

    Add PizzaInfoService

---
+++ Pizzaria/src/main/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/services/PizzaInfoService.java
@@ -0,0 +1,78 @@
+package by.it_academy.jd2.Mk_JD2_92_22.pizza.services;
+
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.api.IPizzaInfo;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.dao.PizzaInfoDaoSingleton;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.dao.api.IPizzaInfoDao;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.dao.entity.PizzaInfo;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.services.api.IPizzaInfoService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PizzaInfoService implements IPizzaInfoService {
+
+    private final IPizzaInfoDao dao;
+
+    public PizzaInfoService() {
+        this.dao = PizzaInfoDaoSingleton.getInstance();
+    }
+
+    @Override
+    public IPizzaInfo read(long id) {
+
+        return dao.read(id);
+    }
+
+    @Override
+    public List<IPizzaInfo> get() {
+
+        return dao.get();
+    }
+
+    @Override
+    public IPizzaInfo create(IPizzaInfo item) {
+
+        return new PizzaInfo(0,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                item.getName(),
+                item.getDescription(),
+                item.getSize());
+    }
+
+    @Override
+    public IPizzaInfo update(long id, LocalDateTime dtUpdate/*дата последнено изменения*/,
+                             IPizzaInfo item/* dto БЕЗ ид и дт, только параметры для изменения*/) {
+        IPizzaInfo read = dao.read(id);
+
+        if (read == null){
+            throw new IllegalArgumentException("Пицца не найдена");
+        }
+        if (read.getDtUpdate().isEqual(dtUpdate)){
+            throw new IllegalArgumentException("Пицца к сожелению кем-то отредактирована");
+        }
+
+        IPizzaInfo updatePizza = new PizzaInfo(0,
+                read.getDtCreate(),
+                LocalDateTime.now(),
+                item.getName(),
+                item.getDescription(),
+                item.getSize());
+
+        return dao.update(id, dtUpdate, updatePizza);
+    }
+
+    @Override
+    public void delete(long id, LocalDateTime dtUpdate) {
+
+        IPizzaInfo read = dao.read(id);
+
+        if (read == null){
+            throw new IllegalArgumentException("Пицца не найдена");
+        }
+        if (!read.getDtUpdate().isEqual(dtUpdate)){
+            throw new IllegalArgumentException("Не удалось удалить, пицца было кем-то отредактирована");
+        }
+        dao.delete(id, dtUpdate);
+    }
+}
---
+++ Pizzaria/src/main/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/services/api/IPizzaInfoService.java
@@ -0,0 +1,6 @@
+package by.it_academy.jd2.Mk_JD2_92_22.pizza.services.api;
+
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.api.IPizzaInfo;
+
+public interface IPizzaInfoService extends IService<IPizzaInfo> {
+}
