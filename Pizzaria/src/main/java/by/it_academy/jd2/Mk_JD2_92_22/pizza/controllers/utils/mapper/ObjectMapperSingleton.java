b082bdcbdd333c303a444aa14cda1fa59cedbb1d
KuzmaEV <yaugen93@yandex.ru>
2022-11-20 10:15:57 +0300

    Add ObjectMapperSingleton

---
+++ Pizzaria/src/main/java/by/it_academy/jd2/Mk_JD2_92_22/pizza/controllers/utils/mapper/ObjectMapperSingleton.java
@@ -0,0 +1,37 @@
+package by.it_academy.jd2.Mk_JD2_92_22.pizza.controllers.utils.mapper;
+
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.controllers.utils.mapper.serializer.LocalDateTimeDeserializer;
+import by.it_academy.jd2.Mk_JD2_92_22.pizza.controllers.utils.mapper.serializer.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.time.LocalDateTime;
+
+public class ObjectMapperSingleton {
+
+    private static ObjectMapperSingleton instance;
+    private final ObjectMapper mapper;
+
+    public ObjectMapperSingleton() {
+        mapper = new ObjectMapper();
+
+        SimpleModule customModule = new SimpleModule();
+        customModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        customModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+
+        mapper.registerModule(customModule);
+        mapper.registerModule(new JavaTimeModule());
+
+    }
+
+    public static ObjectMapper getInstance(){
+        if (instance == null){
+            synchronized (ObjectMapperSingleton.class){
+                    instance = new ObjectMapperSingleton();
+
+            }
+        }
+        return instance.mapper;
+    }
+}
