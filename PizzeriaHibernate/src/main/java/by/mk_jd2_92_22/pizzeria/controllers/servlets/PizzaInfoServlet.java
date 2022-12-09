d1f7c953405bfef573157accba2b95fb223b0f3e
KuzmaEV <yaugen93@yandex.ru>
2022-12-09 16:34:54 +0300

    Added spring-webmvc, converted PizzaInfoServlet to spring

--- PizzeriaHibernate/src/main/java/by/mk_jd2_92_22/pizzeria/controllers/servlets/PizzaInfoServlet.java
+++ PizzeriaHibernate/src/main/java/by/mk_jd2_92_22/pizzeria/controllers/servlets/PizzaInfoServlet.java
@@ -1,118 +1,81 @@
 package by.mk_jd2_92_22.pizzeria.controllers.servlets;
 
-import by.mk_jd2_92_22.pizzeria.controllers.utils.mapper.ObjectMapperSingleton;
 import by.mk_jd2_92_22.pizzeria.dao.entity.api.IPizzaInfo;
 import by.mk_jd2_92_22.pizzeria.services.api.IPizzaInfoService;
 import by.mk_jd2_92_22.pizzeria.services.dto.PizzaInfoDTO;
-import by.mk_jd2_92_22.pizzeria.services.singleton.PizzaInfoServiceSingleton;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
-@WebServlet(name = "PizzaInfoServlet", urlPatterns = "/pizza_info")
-public class PizzaInfoServlet extends HttpServlet {
+@RestController
+@RequestMapping("/pizza_info")
+public class PizzaInfoServlet{
 
     private final IPizzaInfoService service;
-    private final ObjectMapper mapper;
 
 
-    public PizzaInfoServlet() {
-        this.service = PizzaInfoServiceSingleton.getInstance();
-        this.mapper = ObjectMapperSingleton.getInstance();
+    public PizzaInfoServlet(IPizzaInfoService service) {
+        this.service = service;
+
     }
 
     //Read
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @GetMapping
+    @RequestMapping("/{id}")
+    protected ResponseEntity<IPizzaInfo> get(@PathVariable long id){
 
-        String stringId = req.getParameter("id");
-        String json;
-
-
-        if (stringId == null || stringId.isBlank()){
-            json = mapper.writeValueAsString(service.get());
-            resp.getWriter().write(json);
-        } else {
-            long id;
-            try {
-                id = Long.parseLong(stringId.trim());
-            } catch (NumberFormatException e){
-
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-
-                return;//завершает работу метода, сервлета
-            }
-            if (service.read(id) == null){
-                resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
-            } else {
-                json = mapper.writeValueAsString(service.read(id));
-                resp.getWriter().write(json);
-            }
+        return ResponseEntity.ok(service.read(id));
         }
-        }
+
+        //Read List
+    @GetMapping
+    protected ResponseEntity<List<IPizzaInfo>> getList(){
+
+        return ResponseEntity.ok(service.get());
+    }
 
 
     //Create
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @PostMapping
+    protected ResponseEntity<IPizzaInfo> doPost(@RequestBody PizzaInfoDTO dto){
 
-        PizzaInfoDTO dto = mapper.readValue(req.getInputStream(), PizzaInfoDTO.class);
-        if (dto.getName() == null){
-            throw new IllegalStateException("При создании пиццы, нужно ввести Имя");
-        }
-        if (dto.getDescription() == null){
-            throw new IllegalStateException("При создании пиццы, нужно ввести Описание");
-        }
-        if (dto.getSize() == 0){
-            throw new IllegalStateException("При создании пиццы, нужно ввести Размер");
-        }
-
-
-        String pizzaInfoCreate = mapper.writeValueAsString(service.create(dto));
-        resp.getWriter().write(pizzaInfoCreate);
-
-        resp.setStatus(HttpServletResponse.SC_CREATED);
-
+        IPizzaInfo pizzaInfo = service.create(dto);
+        return new ResponseEntity<>(pizzaInfo, HttpStatus.CREATED);
     }
 
     //UPDATE
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @PutMapping("/{id}/dt_update/{dt_update}")
+    protected ResponseEntity<IPizzaInfo> doPut(@PathVariable long id,
+                                               @PathVariable ("dt_update") long dtUpdateRow,
+                                               @RequestBody PizzaInfoDTO dto){
 
-        String idString = req.getParameter("id");
-        long id = Long.parseLong(idString.trim());
+        LocalDateTime dtUpdate = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(dtUpdateRow),
+                ZoneId.of("UTC")
+        );
 
-        LocalDateTime dtUpdate = mapper.readValue(req.getParameter("dtUpdate"), LocalDateTime.class);
-
-        PizzaInfoDTO dto = mapper.readValue(req.getInputStream(), PizzaInfoDTO.class);
-
-
-
-        IPizzaInfo update = service.update(id, dtUpdate, dto);
-        resp.getWriter().write(mapper.writeValueAsString(update));
-
-        resp.setStatus(HttpServletResponse.SC_OK);
+        return ResponseEntity.ok(service.update(id, dtUpdate, dto));
 
     }
 
     //DELETE
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @DeleteMapping("/{id}/dt_update/{dt_update}")
+    protected ResponseEntity<?> doDelete(@PathVariable long id,
+                                         @PathVariable ("dt_update") long dtUpdateRow){
 
-        long id = Long.parseLong(req.getParameter("id"));
-        String dtUpdateString = req.getParameter("dtUpdate");
-
-        LocalDateTime dtUpdate = mapper.readValue(dtUpdateString, LocalDateTime.class);
-
+        LocalDateTime dtUpdate = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(dtUpdateRow),
+                ZoneId.of("UTC")
+        );
 
         service.delete(id, dtUpdate);
 
-        resp.setStatus(HttpServletResponse.SC_OK);
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 }
