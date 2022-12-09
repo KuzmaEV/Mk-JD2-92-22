0a26fbc0e8dcc119bca57676061590112adaa5d0
KuzmaEV <yaugen93@yandex.ru>
2022-12-09 17:29:13 +0300

    MenuServlet converted to Spring

--- PizzeriaHibernate/src/main/java/by/mk_jd2_92_22/pizzeria/controllers/servlets/MenuServlet.java
+++ PizzeriaHibernate/src/main/java/by/mk_jd2_92_22/pizzeria/controllers/servlets/MenuServlet.java
@@ -1,129 +1,75 @@
 package by.mk_jd2_92_22.pizzeria.controllers.servlets;
 
-import by.mk_jd2_92_22.pizzeria.controllers.utils.mapper.ObjectMapperSingleton;
 import by.mk_jd2_92_22.pizzeria.dao.entity.api.IMenu;
 import by.mk_jd2_92_22.pizzeria.services.dto.MenuDTO;
 import by.mk_jd2_92_22.pizzeria.services.api.IMenuService;
-import by.mk_jd2_92_22.pizzeria.services.singleton.MenuServiceSingleton;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
 
-@WebServlet(name = "MenuServlet", urlPatterns = "/menu")
-public class MenuServlet extends HttpServlet {
+@RestController
+@RequestMapping("/menu")
+public class MenuServlet{
 
     private final IMenuService service;
-    private final ObjectMapper mapper;
 
 
-    public MenuServlet(){
-        service = MenuServiceSingleton.getInstance();
-        mapper = ObjectMapperSingleton.getInstance();
+    public MenuServlet(IMenuService service){
+        this.service = service;
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @GetMapping
+    @RequestMapping("/{id}")
+    protected ResponseEntity<IMenu> get(@PathVariable long id){
 
-        String idString = req.getParameter("id");
-
-        if (idString == null || idString.isBlank()){
-            resp.getWriter().write(mapper.writeValueAsString(service.get()));
-            resp.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            long id;
-            try {
-                id = Long.parseLong(idString.trim());
-            } catch (NumberFormatException e) {
-                resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                throw new IllegalArgumentException(e.getMessage());
-            }
-            resp.getWriter().write(mapper.writeValueAsString(service.read(id)));
-            resp.setStatus(HttpServletResponse.SC_OK);
-            }
+        return ResponseEntity.ok(service.read(id));
         }
 
+    @GetMapping
+    protected ResponseEntity<List<IMenu>> getList(){
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        return ResponseEntity.ok(service.get());
+    }
 
-        MenuDTO dto = mapper.readValue(req.getInputStream(), MenuDTO.class);
 
-        if (dto.getName() == null){
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalStateException("Не указано название");
-        }
+    @PostMapping
+    protected ResponseEntity<IMenu> doPost(@RequestBody MenuDTO dto){
+
         IMenu menu = service.create(dto);
-
-        resp.getWriter().write(mapper.writeValueAsString(menu));
-        resp.setStatus(HttpServletResponse.SC_CREATED);
+        return new ResponseEntity<>(menu, HttpStatus.CREATED);
 
     }
 
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @PutMapping("/{id}/dt_update/{dt_update}")
+    protected ResponseEntity<IMenu> doPut(@PathVariable long id,
+                                          @PathVariable("dt_update") long dtUpdateRow,
+                                          @RequestBody MenuDTO dto){
 
-        long id;
+        LocalDateTime dtUpdate = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(dtUpdateRow),
+                ZoneId.of("UTC")
+        );
 
-        String idString = req.getParameter("id");
-        if (idString == null || idString.isBlank()){
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalArgumentException("Не указано id");
-        }
+        return ResponseEntity.ok(service.update(id, dtUpdate, dto));
 
-        try{
-            id = Long.parseLong(idString.trim());
-        } catch (NumberFormatException e) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalStateException(e.getMessage());
-        }
-
-        LocalDateTime dtUpdate = mapper.readValue(req.getParameter("dtUpdate"), LocalDateTime.class);
-
-        if (dtUpdate == null){
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalStateException("Не указано dtUpdate");
-        }
-
-        MenuDTO dto = mapper.readValue(req.getInputStream(), MenuDTO.class);
-
-        IMenu updateMenu = service.update(id, dtUpdate, dto);
-
-        resp.getWriter().write(mapper.writeValueAsString(updateMenu));
-        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
     }
 
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    @DeleteMapping("/{id}/dt_update/{dt_update}")
+    protected ResponseEntity<?> doDelete(@PathVariable long id,
+                                         @PathVariable("dt_update") long dtUpdateRow){
 
-        long id;
+        LocalDateTime dtUpdate = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(dtUpdateRow),
+                ZoneId.of("UTC")
+        );
 
-        String idString = req.getParameter("id");
-        if (idString == null || idString.isBlank()){
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalStateException("Не указано id");
-        }
-
-        try{
-            id = Long.parseLong(idString.trim());
-        } catch (NumberFormatException e) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalStateException(e.getMessage());
-        }
-
-        LocalDateTime dtUpdate = mapper.readValue(req.getParameter("dtUpdate"), LocalDateTime.class);
-
-        if (dtUpdate == null){
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            throw new IllegalArgumentException("Не указано dtUpdate");
-        }
-
-            service.delete(id, dtUpdate);
-            resp.setStatus(HttpServletResponse.SC_OK);
+        service.delete(id, dtUpdate);
+        return new ResponseEntity<>(HttpStatus.OK);
 
     }
 }
