0957a026e3e17ebae5dbaf15ddcadd19689b656f
KuzmaEV <yaugen93@yandex.ru>
2022-12-18 21:18:04 +0300

    Setting ObjectMapper : LocalDateTime -> long Millis

---
+++ Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/controllers/utils/mapper/ObjectMapperSingleton.java
@@ -0,0 +1,40 @@
+package by.mk_jd2_92_22.foodCounter.controllers.utils.mapper;
+
+import by.mk_jd2_92_22.foodCounter.controllers.utils.mapper.serializer.LocalDateTimeDeserializer;
+import by.mk_jd2_92_22.foodCounter.controllers.utils.mapper.serializer.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.time.LocalDateTime;
+
+
+public class ObjectMapperSingleton {
+
+    private static ObjectMapperSingleton instance;
+    private final ObjectMapper mapper;
+
+    public ObjectMapperSingleton() {
+        mapper = new ObjectMapper();
+
+        SimpleModule customModule = new SimpleModule();
+        customModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        customModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(customModule);
+        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+    }
+
+    public static ObjectMapper getInstance(){
+        if (instance == null){
+            synchronized (ObjectMapperSingleton.class){
+                    instance = new ObjectMapperSingleton();
+
+            }
+        }
+        return instance.mapper;
+    }
+}
