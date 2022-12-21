d7529085b5bddb81f42d64bfe89c2dc6ab652e98
KuzmaEV <yaugen93@yandex.ru>
2022-12-21 13:38:19 +0300

    Added column Product

--- Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/services/dto/FoodDiaryDTO.java
+++ Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/services/dto/FoodDiaryDTO.java
@@ -1,35 +1,38 @@
 package by.mk_jd2_92_22.foodCounter.services.dto;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class FoodDiaryDTO {
 
-    private UUID product;
-    private UUID dish;
+    private LocalDateTime dtSupply;
+    private Product product;
+    private Dish dish;
     private int weight;
 
     public FoodDiaryDTO() {
     }
 
-    public FoodDiaryDTO(UUID product, UUID dish, int weight) {
+    public FoodDiaryDTO(LocalDateTime dtSupply, Product product, Dish dish, int weight) {
+        this.dtSupply = dtSupply;
         this.product = product;
         this.dish = dish;
         this.weight = weight;
     }
 
-    public UUID getProduct() {
+    public Product getProduct() {
         return product;
     }
 
-    public void setProduct(UUID product) {
+    public void setProduct(Product product) {
         this.product = product;
     }
 
-    public UUID getDish() {
+    public Dish getDish() {
         return dish;
     }
 
-    public void setDish(UUID dish) {
+    public void setDish(Dish dish) {
         this.dish = dish;
     }
 
@@ -39,5 +42,37 @@ public class FoodDiaryDTO {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public LocalDateTime getDtSupply() {
+        return dtSupply;
+    }
+
+    public void setDtSupply(LocalDateTime dtSupply) {
+        this.dtSupply = dtSupply;
+    }
+
+    public class Product{
+        private UUID uuid;
+
+        public UUID getUuid() {
+            return uuid;
+        }
+
+        public void setUuid(UUID uuid) {
+            this.uuid = uuid;
+        }
+    }
+
+    public class Dish{
+        private UUID uuid;
+
+        public UUID getUuid() {
+            return uuid;
+        }
+
+        public void setUuid(UUID uuid) {
+            this.uuid = uuid;
+        }
     }
 }
