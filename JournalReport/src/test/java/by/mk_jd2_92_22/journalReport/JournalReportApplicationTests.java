4aa7f67bffb9c8effcbd1692742c32e53aa28e7d
KuzmaEV <yaugen93@yandex.ru>
2022-12-23 20:27:08 +0300

    Create project

---
+++ JournalReport/src/test/java/by/mk_jd2_92_22/journalReport/JournalReportApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mk_jd2_92_22.journalReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
