8f77fe6ae216babedf51e73c3b6b28a6cb30c35c
KuzmaEV <yaugen93@yandex.ru>
2022-12-29 22:43:21 +0300

    create JWT Token

---
+++ Security/src/main/java/by/mk_jd2_92_22/userSecurity/security/JwtFilter.java
@@ -0,0 +1,80 @@
+package by.mk_jd2_92_22.userSecurity.security;
+
+import by.mk_jd2_92_22.userSecurity.services.CustomUserDetailsService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.logging.log4j.util.Strings.isEmpty;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+    private final CustomUserDetailsService detailsService;
+
+    public JwtFilter(JwtProvider jwtProvider, CustomUserDetailsService detailsService) {
+        this.jwtProvider = jwtProvider;
+        this.detailsService = detailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = header.split(" ")[1].trim();
+        if (!jwtProvider.validate(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+//        String token = this.jwtProvider.resolveToken(request);
+//        try {
+//            if (token != null && jwtProvider.validate(token)) {
+//                Authentication authentication = jwtProvider.getAuthentication(token);
+//                if (authentication != null) {
+//                    SecurityContextHolder.getContext().setAuthentication(authentication);
+//                }
+//            }
+//        } catch (JwtAuthenticationException e) {
+//            SecurityContextHolder.clearContext();
+//            ((HttpServletResponse) response).sendError(e.getHttpStatus().value());
+//            throw new JwtAuthenticationException("JWT expired or invalid");
+//        }
+
+        UserDetails userDetails = detailsService
+                .loadUserByUsername(jwtProvider.getUsername(token));
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails == null ?
+                        List.of() : userDetails.getAuthorities()
+        );
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ Security/src/main/java/by/mk_jd2_92_22/userSecurity/security/JwtProvider.java
@@ -0,0 +1,96 @@
+package by.mk_jd2_92_22.userSecurity.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.expiration}")
+    private long validityToMilliseconds;
+    @Value("${jwt.issuer}")
+    private String jwtIssuer;
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+
+    public String createToken(String username){
+
+//        Claims claims = Jwts.claims().setSubject(username);
+//        claims.put("role", role);
+        Date now = new Date();
+        Date validity = new Date(now.getTime()+ TimeUnit.MINUTES.toMillis(validityToMilliseconds ));
+
+        return Jwts.builder()
+//                .setClaims(claims)
+                .setSubject(username)
+                .setIssuer(jwtIssuer)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+    public String getUsername(String token){
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public Date getExpirationDate(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody().getExpiration();
+    }
+    public boolean validate(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            //logger.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            //logger.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            //logger.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            //logger.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            //logger.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+    }
+//    public boolean validateToken(String token){
+//        try {
+//            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+//            return !claimsJws.getBody().getExpiration().before(new Date());
+//        } catch (JwtException | IllegalArgumentException e) {
+//            throw new JwtAuthenticationException("JWT is expired or invalid", HttpStatus.UNAUTHORIZED);
+//        }
+//
+//    }
+
+//    public Authentication getAuthentication(String token){
+//        UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUsername(token));
+//        return new UsernamePasswordAuthenticationToken(userDetails,"", userDetails.getAuthorities());
+//    }
+
+//    public String resolveToken(HttpServletRequest request){
+//        return request.getHeader(HttpHeaders.AUTHORIZATION);
+//    }
+
+
+}
