3747e40c71139aedf85c478b0e337b989fc88cc9
KuzmaEV <yaugen93@yandex.ru>
2022-12-29 22:42:10 +0300

    create user security

---
+++ Security/src/main/java/by/mk_jd2_92_22/userSecurity/services/api/IUserService.java
@@ -0,0 +1,7 @@
+package by.mk_jd2_92_22.userSecurity.services.api;
+
+import by.mk_jd2_92_22.userSecurity.model.UserMe;
+import by.mk_jd2_92_22.userSecurity.model.UserCreate;
+
+public interface IUserService extends IService<UserMe, UserCreate> {
+}
