88e4484715cacf3c52103b962fad447acdc0ca5c
KuzmaEV <yaugen93@yandex.ru>
2022-12-29 22:48:26 +0300

    create config security

---
+++ Security/src/main/java/by/mk_jd2_92_22/userSecurity/security/config/SecurityConfig.java
@@ -0,0 +1,108 @@
+package by.mk_jd2_92_22.userSecurity.security.config;
+
+import by.mk_jd2_92_22.userSecurity.security.JwtFilter;
+import by.mk_jd2_92_22.userSecurity.services.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig {
+
+    private final JwtFilter filter;
+
+
+    public SecurityConfig(JwtFilter filter) {
+        this.filter = filter;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    // Бин для настройки доступа
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http = http.cors().and().csrf().disable();
+
+        // Set session management to stateless
+        http = http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and();
+
+        // Set unauthorized requests exception handler
+        http = http
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, ex) -> response.sendError(
+                                HttpServletResponse.SC_UNAUTHORIZED,
+                                ex.getMessage()))
+                .and();
+
+        http.authorizeRequests()
+                .antMatchers("/public/**").permitAll()
+                .antMatchers("/api/v1/users/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(
+                filter,
+                UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http)
+//            throws Exception {
+//        return http.getSharedObject(AuthenticationManagerBuilder.class)
+//                .userDetailsService(userService)
+//                .passwordEncoder(passwordEncoder())
+//                .and()
+//                .build();
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity,
+                                                       UserDetailsService userDetailsService,
+                                                       BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+
+
+//    @Autowired
+//    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+
+//    //Для связи с базай
+//    @Bean
+//    protected DaoAuthenticationProvider daoAuthenticationProvider(){
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+//        daoAuthenticationProvider.setUserDetailsService(this.userService);
+//        return daoAuthenticationProvider;
+//    }
+
+
+
+}
