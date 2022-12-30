0db8a58c92de17dcf8f69a15e9c7245192063781
KuzmaEV <yaugen93@yandex.ru>
2022-12-30 10:38:46 +0300

    delete

--- UserSecurity/src/main/java/by/mk_jd2_92_22/userSecurity/model/Role.java
+++
@@ -1,5 +0,0 @@
-package by.mk_jd2_92_22.userSecurity.model;
-
-public enum Role {
-    USER, ADMIN
-}
--- UserSecurity/src/main/java/by/mk_jd2_92_22/userSecurity/model/Status.java
+++
@@ -1,7 +0,0 @@
-package by.mk_jd2_92_22.userSecurity.model;
-
-public enum Status {
-    WAITING_ACTIVATION,
-    ACTIVATED,
-    DEACTIVATED
-}
