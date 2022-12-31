20a5598441b0085934c42f5c47c1b11c167d62a9
KuzmaEV <yaugen93@yandex.ru>
2022-12-31 11:13:21 +0300

    create project

---
+++ audit-service/src/test/java/by/mk_jd2_92_22/auditservice/AuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mk_jd2_92_22.auditservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
