df8bf4cec9ca8af132456861c08cc6d3a974e02f
KuzmaEV <yaugen93@yandex.ru>
2023-01-05 10:20:36 +0300

    delete

--- Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/services/mappers/IMapperPageDTO.java
+++
@@ -1,8 +0,0 @@
-package by.mk_jd2_92_22.foodCounter.services.mappers;
-
-import by.mk_jd2_92_22.foodCounter.services.dto.IPageDTO;
-import org.springframework.data.domain.Page;
-
-public interface IMapperPageDTO<T> {
-    IPageDTO<T> mapper(Page<T> page);
-}
