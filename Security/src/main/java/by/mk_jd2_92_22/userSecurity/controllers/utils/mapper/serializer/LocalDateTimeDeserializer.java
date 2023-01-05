fbbeb7ce712b6678aaecc3882dfdc7749c11c57f
KuzmaEV <yaugen93@yandex.ru>
2023-01-05 23:33:19 +0300

    created converter

--- Security/src/main/java/by/mk_jd2_92_22/userSecurity/controllers/utils/mapper/serializer/LocalDateTimeDeserializer.java
+++ Security/src/main/java/by/mk_jd2_92_22/userSecurity/controllers/utils/mapper/serializer/LocalDateTimeDeserializer.java
@@ -3,12 +3,14 @@ package by.mk_jd2_92_22.userSecurity.controllers.utils.mapper.serializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
+@JsonComponent
 public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
     @Override
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
