aefe7c37ee7ab0d538866b3eaac532fcf411f150
KuzmaEV <yaugen93@yandex.ru>
2023-01-09 16:05:56 +0300

    delete

--- audit-service/src/main/java/by/mk_jd2_92_22/auditservice/dao/AuditRepository.java
+++
@@ -1,9 +0,0 @@
-package by.mk_jd2_92_22.auditservice.dao;
-
-import by.mk_jd2_92_22.auditservice.model.Audit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AuditRepository extends JpaRepository<Audit, UUID> {
-}
