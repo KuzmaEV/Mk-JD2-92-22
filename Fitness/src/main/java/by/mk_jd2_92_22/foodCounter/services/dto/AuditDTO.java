84612b13d6e4a8959e378babd9560f1f2cb4d673
KuzmaEV <yaugen93@yandex.ru>
2023-01-10 21:51:25 +0300

    added Audit

---
+++ Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/services/dto/AuditDTO.java
@@ -0,0 +1,43 @@
+package by.mk_jd2_92_22.foodCounter.services.dto;
+
+import java.util.UUID;
+
+public class AuditDTO {
+
+    private UUID user;
+    private String text;
+    private Type type;
+
+    public AuditDTO() {
+    }
+
+    public AuditDTO(UUID user, String text, Type type) {
+        this.user = user;
+        this.text = text;
+        this.type = type;
+    }
+
+    public UUID getUser() {
+        return user;
+    }
+
+    public void setUser(UUID user) {
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
---
+++ Fitness/src/main/java/by/mk_jd2_92_22/foodCounter/services/dto/Type.java
@@ -0,0 +1,9 @@
+package by.mk_jd2_92_22.foodCounter.services.dto;
+
+public enum Type {
+    PRODUCT,
+    RECIPE,
+    USER,
+    JOURNAL_FOOD,
+    REPORT
+}
